18c85de98ef5ae736084b2abca1ff19dc1a2da7b
Omar Chong <al221811717@gmail.com>
2022-02-28 09:21:08 -0600

    cambio welcome

--- resources/views/system/clientes/index.blade.php
+++ resources/views/system/clientes/index.blade.php
@@ -60,35 +60,35 @@
 
                         </div>
                         <div class="modal-body">
-                                <div class="card">
-                                    <h5 class="card-header">Alta clientes</h5>
-                                    <div class="card-body">
-                                        <form>
-                                            <div class="form-row">
+                            <div class="card">
+                                <h5 class="card-header">Alta clientes</h5>
+                                <div class="card-body">
+                                    <form>
+                                        <div class="form-row">
 
-                                                <div class="col-md-8">
-                                                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                                    <div class="form-group">
-                                                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-8">
-                                                    <div class="form-group">
-                                                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                                                        <input type="password" class="form-control" id="exampleInputPassword1">
-                                                    </div>
-
-                                                </div>
-                                                <div class="mb-3 form-check">
-                                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                            <div class="col-md-8">
+                                                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                                <div class="form-group">
+                                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                                                 </div>
                                             </div>
-                                            <button type="submit" class="btn btn-primary">Submit</button>
-                                        </form>
-                                    </div>
+                                            <div class="col-md-8">
+                                                <div class="form-group">
+                                                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword1">
+                                                </div>
+
+                                            </div>
+                                            <div class="mb-3 form-check">
+                                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                            </div>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    </form>
                                 </div>
+                            </div>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,22 @@
-<h2>Hola DSW</h2>
-<h4>Hola soy Crithian Rodriguez</h4>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- importamos a tailwind -->
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>Document</title>
+</head>
+
+<body>
+    <section>
+        <div class="">
+
+        </div>
+
+    </section>
+</body>
+
+</html>
